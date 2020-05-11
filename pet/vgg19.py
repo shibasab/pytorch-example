b0efc32e101f55d542efc2a7b69223b0e7546e23
shibasab <yakyu.hira89@outlook.jp>
2020-05-11 20:47:31 +0900

    vgg

---
+++ pet/vgg19.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VGG19(nn.Module):
+    def __init__(self, in_channel, num_classes):
+        super(VGG19, self).__init__()
+        self.in_channel = in_channel
+        self.num_classes = num_classes
+
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_channel, out_channels=64,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=1),
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=128, out_channels=128,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=1),
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256, out_channels=256,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256, out_channels=256,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256, out_channels=256,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=1),
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=1),
+        )
+        self.block5 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                      kernel_size=(3, 3), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=1),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=512*8*8, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=4096, out_features=num_classes)
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = F.softmax(x, dim=1)
+
+        return x
